55fc9deb3fcc850c3a1361314ab3695fc4babded
Cesar <cesarbiods@gmail.com>
2016-12-05 16:35:40 -0500

    Function 4 done

--- thing.hs
+++ thing.hs
@@ -12,3 +12,11 @@ replace new (lower, upper) list  = x ++ new ++ w
   where
     (x, y) = splitAt lower list
     (z, w) = splitAt ((upper - lower)+ 1) y
+
+--found this improvised algorithm at http://stackoverflow.com/questions/21276844/prime-factors-in-haskell
+factor :: Int -> [Int]
+factor 1 = []
+factor n
+  | factors == []  = [n]
+  | otherwise = factors ++ factor (n `div` (head factors))
+  where factors = take 1 $ filter (\x -> (n `mod` x) == 0) [2 .. n-1]
