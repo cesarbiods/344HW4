91cfaeb87b7a99502b03247bf9ecdf001bc76727
Cesar <cesarbiods@gmail.com>
2016-12-04 21:37:40 -0500

    Function 3 done

--- thing.hs
+++ thing.hs
@@ -6,3 +6,9 @@ dub [] = []
 dub (x:xs)   
     | x == 'a' = x:x:(dub xs)  
     | otherwise = x:(dub xs)
+
+replace :: [a] -> (Int, Int) -> [a] -> [a]
+replace new (lower, upper) list  = x ++ new ++ w
+  where
+    (x, y) = splitAt lower list
+    (z, w) = splitAt ((upper - lower)+ 1) y
